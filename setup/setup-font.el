769eeda176e698c45a6ba23e2fb01892c9f2c344
ansterd <ansterd@nate.com>
2014-04-22 01:55:14 +0900

    Update: NanumGothic Font

--- setup/setup-font.el
+++ setup/setup-font.el
@@ -1,3 +1,11 @@
+(setq nanum-font-dir "/usr/share/fonts/truetype/nanum")
+
+(when (file-directory-p nanum-font-dir)
+  (set-fontset-font "fontset-default" '(#x1100 . #xffdc)
+		    "NanumGothic"))
+
+(setq face-font-rescale-alist
+      '((".*nanum.*" . 1.0)))
 
 
 (provide 'setup-font)
