2700d4ced10a0169ad7592a444beeef24e8987a0
ansterd <ansterd@nate.com>
2014-04-29 13:36:08 +0900

    No Map Capslock at ESC in Emacs config

--- setup/setup-evil.el
+++ setup/setup-evil.el
@@ -2,8 +2,8 @@
 (evil-mode 1)
 
 ;; evil-force-normal-state : [capslock]
-(if (eq window-system 'x)
-    (shell-command "xmodmap -e 'clear Lock' -e 'keycode 66 = F13'"))
-(global-set-key [f13] 'evil-force-normal-state)
+;; (if (eq window-system 'x)
+;;     (shell-command "xmodmap -e 'clear Lock' -e 'keycode 66 = F13'"))
+;; (global-set-key [f13] 'evil-force-normal-state) 
 
 (provide 'setup-evil)
