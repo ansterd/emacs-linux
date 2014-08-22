dfdaeaa0dc7f8cd7e1bf726dbe1c3bc5337f8282
1ambda <1amb4a@gmail.com>
2014-08-23 02:29:12 +0900

    Working: ensigm, auto-comp in scala-mode

--- setup/language-scala.el
+++ setup/language-scala.el
@@ -6,14 +6,20 @@
 (packages-install '(scala-mode2
 		    ensime))
 
+(require 'auto-complete)
+(add-to-list 'ac-modes 'scala-mode)
 
 (require 'scala-mode2)
 (require 'ensime)
 
 (defun custom:scala-mode-config ()
-  (fci-mode 1))
+  (fci-mode 1)
+  (yas-activate-extra-mode 'scala-mode)
+  (add-to-list 'ac-sources 'ac-source-yasnippet))
 
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 (add-hook 'scala-mode-hook 'custom:scala-mode-config)
 
 (provide 'language-scala)
+
+
